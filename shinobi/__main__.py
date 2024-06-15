b667c7e8cb3ff1897f0de5dacf5a7a5c5f48f78a
carrot <68383195+AnnikaV9@users.noreply.github.com>
2024-06-15 14:41:02 +0800

    Update dependencies and minor code design tweaks

--- shinobi/__main__.py
+++ shinobi/__main__.py
@@ -19,32 +19,37 @@ asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 # load and return configuration
 def load_config() -> dict:
     with open("config.yml", "r", encoding="utf-8") as config_file:
-        config: dict = yaml.safe_load(config_file)
+        config = yaml.safe_load(config_file)
 
-    config["nick"]: str = str(random.randint(1000, 9999)) if config["nick"] == "RANDOM" else config["nick"]
-    config["nick"]: str = "{}#{}".format(config["nick"], config["password"]) if config["password"] else config["nick"]
+    config["nick"] = str(random.randint(1000, 9999)) if config["nick"] == "RANDOM" else config["nick"]
+    config["nick"] = f"{config['nick']}#{config['password']}" if config["password"] else config["nick"]
     return config
 
 # create and return logger objects
 def create_logger(name: str, log_file: str) -> object:
-    formatter: object = logging.Formatter("%(asctime)s | %(message)s")
-    handler: object = logging.FileHandler(log_file)
+    formatter = logging.Formatter("%(asctime)s | %(message)s")
+    handler = logging.FileHandler(log_file)
     handler.setFormatter(formatter)
-    logger: object = logging.getLogger(name)
+    logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
     logger.addHandler(handler)
     logger.propagate = False
     return logger
 
 # run connection() coroutine for each channel
-async def main(nick: str, channels: list, server: list) -> None:
-    await asyncio.gather(*[connection(nick, channel, channels.index(channel) , server) for channel in channels])
+async def main(config: dict) -> None:
+    channels = list(dict.fromkeys(config["channels"]))
+    nick, server, join_delay = config["nick"], config["server"], config["join_delay"]
+    await asyncio.gather(*[connection(nick, channel, channels.index(channel) , server, join_delay) for channel in channels])
 
 # connect to server and start ping() and receive() coroutines
-async def connection(nick: str, channel: str, channel_no: int, server: str) -> None:
-    logger: object = create_logger(channel, f"logs/{channel}.log")
+async def connection(nick: str, channel: str, num_channels: int, server: str, join_delay: int) -> None:
+    logger = create_logger(channel, f"logs/{channel}.log")
+    global logger_objects
     logger_objects.append(logger)
-    await asyncio.sleep(channel_no * config["join_delay"]) if channel_no != 0 else None
+    if num_channels != 0:
+        await asyncio.sleep(num_channels * join_delay)
+
     async with websockets.connect(server) as ws:
         await ws.send(json.dumps({"cmd": "join", "channel": channel, "nick": nick}))
         await asyncio.gather(ping_loop(ws), receive_loop(ws, logger))
@@ -58,42 +63,41 @@ async def ping_loop(ws: object) -> None:
 # receive messages and log them
 async def receive_loop(ws: object, logger: object) -> None:
     while True:
-        resp: str = await ws.recv()
-        resp: dict = json.loads(resp)
-        if "cmd" in resp:
-            match resp["cmd"]:
-                case "chat":
-                    resp["trip"] = "NOTRIP" if len(resp.get("trip", "")) < 6 else resp.get("trip", "")
-                    logger.info("[{}][{}] {}".format(resp["trip"], resp["nick"], resp["text"].replace("\n", "<LB>")))
+        resp = await ws.recv()
+        resp = json.loads(resp)
+        match resp["cmd"]:
+            case "chat":
+                resp["trip"] = "NOTRIP" if len(resp.get("trip", "")) < 6 else resp.get("trip", "")
+                logger.info(f"[{resp['trip']}][{resp['nick']}] {resp['text'].replace('\n', '<LB>')}")
 
-                case "info":
-                    logger.info(resp["text"].replace("\n", "<LB>"))
+            case "info":
+                logger.info(resp["text"].replace("\n", "<LB>"))
 
-                case "emote":
-                    resp["trip"] = "NOTRIP" if len(resp.get("trip", "")) < 6 else resp.get("trip", "")
-                    logger.info("[{}][{}] {}".format(resp["trip"], resp["nick"], resp["text"].replace("\n", "<LB>")))
+            case "emote":
+                resp["trip"] = "NOTRIP" if len(resp.get("trip", "")) < 6 else resp.get("trip", "")
+                logger.info(f"[{resp['trip']}][{resp['nick']}] {resp['text'].replace('\n', '<LB>')}")
 
-                case "onlineAdd":
-                    logger.info("{} joined".format(resp["nick"]))
+            case "onlineAdd":
+                logger.info(f"{resp['nick']} joined")
 
-                case "onlineRemove":
-                    logger.info("{} left".format(resp["nick"]))
+            case "onlineRemove":
+                logger.info(f"{resp['nick']} left")
 
-                case "onlineSet":
-                    logger.info("Online: {}".format(", ".join(resp["nicks"])))
-                    print("Connected to channel: {}".format(resp["channel"]))
+            case "onlineSet":
+                logger.info(f"Online: {', '.join(resp['nicks'])}")
+                print(f"Connected to channel: {resp['channel']}")
 
 # run main()
 if __name__ == "__main__":
-    config: dict = load_config()
+    config = load_config()
     logging.basicConfig(filemode="a")
-    logger_objects: list = []
+    logger_objects = []
 
     # register exit handler
     atexit.register(lambda: [logger.info("Connection closed") for logger in logger_objects])
 
     try:
-        asyncio.run(main(config["nick"], list(dict.fromkeys(config["channels"])), config["server"]))
+        asyncio.run(main(config))
 
     except KeyboardInterrupt:
         raise SystemExit
